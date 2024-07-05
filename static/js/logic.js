e227cd3e949662bb392aa79f06245037a081bfa3
Austen <aortmeier20@gmail.com>
2024-07-05 17:02:19 -0500

    entered in code for map

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,5 +1,16 @@
 
 //store url
-const url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson'
+const url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson';
+
+// Create a Leaflet map object.
+var myMap = L.map("map", {
+    center: [44.58, -103.46],
+    zoom: 5, 
+});
+
+// Add a Leaflet tile layer.
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 5,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(myMap)
 
 
