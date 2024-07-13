10a6a0f3807329ca9326f64fb21fa20bca17bcb5
Austen <aortmeier20@gmail.com>
2024-07-13 16:32:14 -0500

    trial and error

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,6 +2,13 @@
 //store url
 const url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson';
 
+// get request
+d3.json(url).then(function(earthquakeData){
+    console.log(earthquakeData);
+    createFeatures(earthquakeData.features)
+});
+
+
 // map
 var myMap = L.map("map", {
     center: [44.58, -103.46],
@@ -13,4 +20,14 @@ var myMap = L.map("map", {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(myMap)
 
-
+// create markers
+function createMarker(feature, latlng) {
+    return L.circlemarker(latlng, {
+        radius: markersize(feature.properties.mag),
+        fillColor: markerColor(feature.geometry.coordinates[2]),
+        color:"#000",
+        weight: 0.5,
+        opacity: 0.5,
+        fillOpacity: 1
+    })
+}
