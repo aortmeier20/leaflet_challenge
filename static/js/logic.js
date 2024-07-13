d41a1e71bcc8694f5093abe424b251dbc3505aff
Austen <aortmeier20@gmail.com>
2024-07-13 17:15:20 -0500

    worked in marker size and marker color

--- static/js/logic.js
+++ static/js/logic.js
@@ -64,14 +64,20 @@ function createMarker(feature, latlng) {
     });
 }
 
+// Increase marker size based on magnitude
+function markerSize(magnitude) {
+    return magnitude * 5;
+}
 
-
-
-
-
-
-
-
+// Change marker color based on depth
+function markerColor(depth) {
+    return depth > 90 ? '#d73027' :
+            depth > 70 ? '#fc8d59' :
+            depth > 50 ? '#fee08b' :
+            depth > 30 ? '#d9ef8b' :
+            depth > 10 ? '#91cf60' :
+                         '#1a9850' ;          
+}
 
 
 function createFeatures(earthquakeData) {
